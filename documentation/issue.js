d40903ce5d524fa86d1615412228347e83144d40
Assaf Shomer <assafshomer@gmail.com>
2015-07-02 16:32:41 +0300

    User object created succesfully

--- documentation/issue.js
+++ documentation/issue.js
@@ -1,11 +1,16 @@
 // var Colu = require('colu-node')
+var bitcoin = require('bitcoinjs-lib');
 var Colu = require('../src/index.js');
 var User = require('../src/user.js');
 
 var privateSeed = 'c507290be50bc29a787af1901ff80e1c9f17e4020ee0a4fd51495ee4424d6150';
 
-var pub = User.getPublicKey(privateSeed);
-console.log("pub:"+pub)
+var hdnode = bitcoin.HDNode.fromSeedHex('c507290be50bca9b887af39019f80e2f9f27e4020ee0a4fe51595ee4424d6151', bitcoin.networks.testnet)
+
+var userid = hdnode.toBase58(false);
+
+var user = new User(userid);
+console.log(user.getId())
 
 var assetData = {
   amount: 1, //Number
@@ -35,7 +40,7 @@ Colu.init('my_company', 'testnet', privateSeed, function (err, colu) {
   console.log('seed: ' + colu.getPrivateSeed())
   var nextAccount = 1 // colu.nextAccount
   console.log('nextAccount: ' + colu.nextAccount)
-  colu.ccIssueFinanced(nextAccount, 1, function (err, data) {
+  colu.ccIssueFinanced(nextAccount, user,assetData, function (err, data) {
     if (err) {
       console.error('error: ' + err)
     } else {
