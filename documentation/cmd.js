431f7cf17a432111531ab7e8dcce3112f020fe79
Assaf Shomer <assafshomer@gmail.com>
2015-07-02 16:20:01 +0300

    generated a user with bitcoin js hdnode function. Added the metadata dudu created

--- documentation/cmd.js
+++ documentation/cmd.js
@@ -9,4 +9,7 @@ console.log('key: ['+wif+']');
 
 var hdnode = bitcoin.HDNode.fromSeedHex('c507290be50bca9b887af39019f80e2f9f27e4020ee0a4fe51595ee4424d6151', bitcoin.networks.testnet)
 
-console.log("hd:"+hdnode.toBase58())
+var user = hdnode.toBase58(false);
+
+console.log("user:"+user)
+
--- documentation/issue.js
+++ documentation/issue.js
@@ -1,7 +1,31 @@
 // var Colu = require('colu-node')
-var Colu = require('../src/index.js')
+var Colu = require('../src/index.js');
+var User = require('../src/user.js');
 
-var privateSeed = 'c507290be50bc29a787af1901ff80e1c9f17e4020ee0a4fd51495ee4424d6150'
+var privateSeed = 'c507290be50bc29a787af1901ff80e1c9f17e4020ee0a4fd51495ee4424d6150';
+
+var pub = User.getPublicKey(privateSeed);
+console.log("pub:"+pub)
+
+var assetData = {
+  amount: 1, //Number
+  reissueable: true, //boolean
+  injectPreviousOutput: false, //boolean
+  divisibility: 0, //Number
+  metadata : { // Optional
+    assetId: "assetId",//String
+    assetName: "assetName",//String
+    issuer: "issuer",//String // Name of the asset issuer
+    description: "description",//String
+    userData : { // an object containing an array of key value type objects
+      meta: [
+          {key: "item id", value: 1, type: "Number"},
+          {key: "key", value: "value", type: "type"},
+      ]
+    } 
+  }
+};
+
 
 Colu.init('my_company', 'testnet', privateSeed, function (err, colu) {
   if (err) {
@@ -11,7 +35,7 @@ Colu.init('my_company', 'testnet', privateSeed, function (err, colu) {
   console.log('seed: ' + colu.getPrivateSeed())
   var nextAccount = 1 // colu.nextAccount
   console.log('nextAccount: ' + colu.nextAccount)
-  colu.issue('bob_' + nextAccount, nextAccount, 1, function (err, data) {
+  colu.ccIssueFinanced(nextAccount, 1, function (err, data) {
     if (err) {
       console.error('error: ' + err)
     } else {
